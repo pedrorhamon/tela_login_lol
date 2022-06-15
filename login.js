1670b6aa71d0fe84cd77c22fb72332198a23c2d0
pedrorhamon <pedrorhamon16@gmail.com>
2022-06-14 22:58:34 -0300

    ajustando js para ações no campo input

--- login.js
+++ login.js
@@ -1,3 +1,16 @@
 const inputs = document.querySelectorAll('input');
 
-inputs.forEach((input) => (input.addEventListener('focus', handleFocus)))
+const handleFocus = ({target}) => {
+    const span = target.previousElementSibling;
+    span.classList.add('span-active');
+}
+
+const handleFocusout = ({target}) => {
+    if(target.value === '') {
+        const span = target.previousElementSibling;
+        span.classList.remove('span-active');
+    }
+}
+
+inputs.forEach((input) => (input.addEventListener('focus', handleFocus)));
+inputs.forEach((input) => (input.addEventListener('focusout', handleFocusout)));
