536aa593b25eb79ae10c1bf1c96458e1e7439441
Pedro Rhamon <pedrorhamon16@gmail.com>
2022-06-28 20:32:12 -0300

    Criando função para senha

--- login.js
+++ login.js
@@ -1,4 +1,6 @@
 const inputs = document.querySelectorAll('input');
+const button = document.querySelectorAll('button_login');
+
 
 const handleFocus = ({target}) => {
     const span = target.previousElementSibling;
@@ -12,5 +14,16 @@ const handleFocusout = ({target}) => {
     }
 }
 
+const handleChange = () => {
+    const [username, password] = inputs;
+
+    if(username.value && password.value.length >= 8) {
+       button.removeAttribute('disabled');
+    } else {
+        button.setAttribute('disabled', '');  
+    }
+}
+
 inputs.forEach((input) => (input.addEventListener('focus', handleFocus)));
 inputs.forEach((input) => (input.addEventListener('focusout', handleFocusout)));
+inputs.forEach((input) => (input.addEventListener('imnput', handleChange)));
